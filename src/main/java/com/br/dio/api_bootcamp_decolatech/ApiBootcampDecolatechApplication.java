a70dce540837f7e5c0bd9f8add057d9aaec4c180
Wiliam102 <ribeirowiliam562@gmail.com>
2025-03-22 15:58:55 -0300

    estrutura inicial projeto

---
+++ src/main/java/com/br/dio/api_bootcamp_decolatech/ApiBootcampDecolatechApplication.java
@@ -0,0 +1,13 @@
+package com.br.dio.api_bootcamp_decolatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBootcampDecolatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBootcampDecolatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/dio/api_bootcamp_decolatech/ApiBootcampDecolatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dio.api_bootcamp_decolatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBootcampDecolatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
